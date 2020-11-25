0da22855b6f38fbb77de7985512022fa074e2f77
Aerondight77 <ho.bailey54@gmail.com>
2020-11-24 22:33:19 -0800

    Implemented ActiveTimeForm.

--- GUI_App7/GUI_App7/ActiveTimeForm.Designer.cs
+++ GUI_App7/GUI_App7/ActiveTimeForm.Designer.cs
@@ -29,23 +29,27 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.txtWeightDiff = new System.Windows.Forms.TextBox();
+            this.txtAddTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTotalTime = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label18 = new System.Windows.Forms.Label();
             this.label19 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnUpdate = new System.Windows.Forms.Button();
-            this.btnReset = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtAddTime = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.txtAddTime);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.btnReset);
@@ -53,7 +57,7 @@
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.txtWeightDiff);
+            this.panel1.Controls.Add(this.txtTotalTime);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.label18);
             this.panel1.Controls.Add(this.label19);
@@ -62,20 +66,103 @@
             this.panel1.Size = new System.Drawing.Size(718, 441);
             this.panel1.TabIndex = 0;
             // 
-            // txtWeightDiff
+            // txtAddTime
             // 
-            this.txtWeightDiff.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
-            this.txtWeightDiff.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtWeightDiff.Enabled = false;
-            this.txtWeightDiff.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWeightDiff.ForeColor = System.Drawing.Color.White;
-            this.txtWeightDiff.Location = new System.Drawing.Point(220, 74);
-            this.txtWeightDiff.Multiline = true;
-            this.txtWeightDiff.Name = "txtWeightDiff";
-            this.txtWeightDiff.Size = new System.Drawing.Size(167, 59);
-            this.txtWeightDiff.TabIndex = 25;
-            this.txtWeightDiff.Text = "_";
-            this.txtWeightDiff.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtAddTime.Location = new System.Drawing.Point(165, 157);
+            this.txtAddTime.Name = "txtAddTime";
+            this.txtAddTime.Size = new System.Drawing.Size(100, 20);
+            this.txtAddTime.TabIndex = 32;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(453, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 18);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Click only on a new day";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnReset.FlatAppearance.BorderSize = 0;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnReset.Location = new System.Drawing.Point(466, 368);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(161, 41);
+            this.btnReset.TabIndex = 30;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.btnUpdate.FlatAppearance.BorderSize = 0;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
+            this.btnUpdate.Location = new System.Drawing.Point(87, 368);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(151, 41);
+            this.btnUpdate.TabIndex = 29;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(38, 266);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(349, 18);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "may need to exercist more.\" -mayoclinic.org/";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(38, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(622, 18);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "If you want to lose weight, maintain weight loss or meet specific fitness goals, " +
+    "you";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(29, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(656, 18);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "\"As a general goal, aim for at least 30 minutes of moderate physical activity eve" +
+    "ry day.";
+            // 
+            // txtTotalTime
+            // 
+            this.txtTotalTime.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.txtTotalTime.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTotalTime.Enabled = false;
+            this.txtTotalTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalTime.ForeColor = System.Drawing.Color.White;
+            this.txtTotalTime.Location = new System.Drawing.Point(220, 74);
+            this.txtTotalTime.Multiline = true;
+            this.txtTotalTime.Name = "txtTotalTime";
+            this.txtTotalTime.Size = new System.Drawing.Size(167, 59);
+            this.txtTotalTime.TabIndex = 25;
+            this.txtTotalTime.Text = "_";
+            this.txtTotalTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label1
             // 
@@ -110,86 +197,27 @@
             this.label19.TabIndex = 15;
             this.label19.Text = "Track the time you have been active today";
             // 
-            // label2
+            // label6
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(29, 230);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(656, 18);
-            this.label2.TabIndex = 26;
-            this.label2.Text = "\"As a general goal, aim for at least 30 minutes of moderate physical activity eve" +
-    "ry day.";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(384, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 18);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "minutes";
             // 
-            // label3
+            // label7
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(38, 248);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(622, 18);
-            this.label3.TabIndex = 27;
-            this.label3.Text = "If you want to lose weight, maintain weight loss or meet specific fitness goals, " +
-    "you";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(38, 266);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(349, 18);
-            this.label4.TabIndex = 28;
-            this.label4.Text = "may need to exercist more.\" -mayoclinic.org/";
-            // 
-            // btnUpdate
-            // 
-            this.btnUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.btnUpdate.FlatAppearance.BorderSize = 0;
-            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnUpdate.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpdate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnUpdate.Location = new System.Drawing.Point(87, 368);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(151, 41);
-            this.btnUpdate.TabIndex = 29;
-            this.btnUpdate.Text = "Update";
-            this.btnUpdate.UseVisualStyleBackColor = false;
-            // 
-            // btnReset
-            // 
-            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.btnReset.FlatAppearance.BorderSize = 0;
-            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnReset.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReset.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnReset.Location = new System.Drawing.Point(466, 368);
-            this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(161, 41);
-            this.btnReset.TabIndex = 30;
-            this.btnReset.Text = "Reset";
-            this.btnReset.UseVisualStyleBackColor = false;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(453, 412);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(184, 18);
-            this.label5.TabIndex = 31;
-            this.label5.Text = "Click only on a new day";
-            // 
-            // txtAddTime
-            // 
-            this.txtAddTime.Location = new System.Drawing.Point(165, 157);
-            this.txtAddTime.Name = "txtAddTime";
-            this.txtAddTime.Size = new System.Drawing.Size(100, 20);
-            this.txtAddTime.TabIndex = 32;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(271, 159);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 18);
+            this.label7.TabIndex = 34;
+            this.label7.Text = "minutes";
             // 
             // ActiveTimeForm
             // 
@@ -201,6 +229,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ActiveTimeForm";
             this.Text = "ActiveTimeForm";
+            this.Load += new System.EventHandler(this.ActiveTimeForm_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -213,7 +242,7 @@
         private System.Windows.Forms.Label label19;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label18;
-        private System.Windows.Forms.TextBox txtWeightDiff;
+        private System.Windows.Forms.TextBox txtTotalTime;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
@@ -221,5 +250,7 @@
         private System.Windows.Forms.TextBox txtAddTime;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
     }
 }
--- GUI_App7/GUI_App7/ActiveTimeForm.cs
+++ GUI_App7/GUI_App7/ActiveTimeForm.cs
@@ -7,14 +7,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace GUI_App7
 {
     public partial class ActiveTimeForm : Form
     {
+        DBAccess objDBAccess = new DBAccess();
+        DataTable dtLogin_Table = new DataTable();
+
+        //public static int ActiveTime;
+
         public ActiveTimeForm()
         {
             InitializeComponent();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (txtAddTime.Text.Equals(""))
+            {
+                MessageBox.Show("Please enter a valid amount of water.");
+            }
+            else
+            {
+                int activeTime = int.Parse(txtAddTime.Text);
+                int activeTimeTotal = int.Parse(txtTotalTime.Text);
+                activeTimeTotal += activeTime;
+
+                string query = "Update Login_Table SET ActiveTime = '" + activeTimeTotal + "' where ID = '" + LoginForm.id + "'";
+
+                SqlCommand updateCommand = new SqlCommand(query);
+
+                updateCommand.Parameters.AddWithValue("@activeTimeTotal", @activeTimeTotal);
+
+                int row = objDBAccess.executeQuery(updateCommand);
+
+                if (row == 1)
+                {
+                    txtTotalTime.Clear();
+                    txtTotalTime.AppendText(activeTimeTotal.ToString());
+                    txtAddTime.Clear();
+                    MessageBox.Show("Active Time data successfully updated.");
+                }
+                else
+                {
+                    MessageBox.Show("Active Time data failed to update.");
+                }
+            }
+        }
+
+        private void ActiveTimeForm_Load(object sender, EventArgs e)
+        {
+            int activeTimeTotal;
+            string query = "SELECT ActiveTime FROM Login_Table where ID = '" + LoginForm.id + "'";
+
+            SqlCommand getCommand = new SqlCommand(query);
+
+            objDBAccess.readDatathroughAdapter(query, dtLogin_Table);
+
+            if (dtLogin_Table.Rows.Count == 1)
+            {
+                activeTimeTotal = int.Parse(dtLogin_Table.Rows[0]["ActiveTime"].ToString());
+
+                objDBAccess.closeConn();
+
+                txtTotalTime.Clear();
+                txtTotalTime.AppendText(activeTimeTotal.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Unable to load Active Time data. Please try again.");
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            int empty = 0;
+            string query = "Update Login_Table SET ActiveTime = '" + @empty + "' where ID = '" + LoginForm.id + "'";
+            SqlCommand updateCommand = new SqlCommand(query);
+
+            updateCommand.Parameters.AddWithValue("@empty", @empty);
+
+            int row = objDBAccess.executeQuery(updateCommand);
+
+            if (row == 1)
+            {
+                txtTotalTime.Clear();
+                txtTotalTime.AppendText(empty.ToString());
+            }
+            else
+            {
+                // Debug purposes.
+                MessageBox.Show("Error in resetting active time data.");
+            }
         }
     }
 }
