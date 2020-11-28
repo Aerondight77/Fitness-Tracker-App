33dff54e3ecdb728c8970a8ac4c0c74ddfdd1f55
Aerondight77 <ho.bailey54@gmail.com>
2020-11-28 00:49:46 -0800

    Implemented Workout Calculator.

--- GUI_App7/GUI_App7/WorkoutCalcForm.cs
+++ GUI_App7/GUI_App7/WorkoutCalcForm.cs
@@ -16,5 +16,50 @@ namespace GUI_App7
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (txtPushUps.Text.Equals(""))
+            {
+                MessageBox.Show("Please enter a value in the push-ups field.");
+            }
+            else if (txtSitUps.Text.Equals(""))
+            {
+                MessageBox.Show("Please enter a value in the sit-ups field.");
+            }
+            else if (txtRunning.Text.Equals(""))
+            {
+                MessageBox.Show("Please enter a value in the jogging/running field.");
+            }
+            else
+            {
+                int pushUps = int.Parse(txtPushUps.Text);
+                int sitUps = int.Parse(txtSitUps.Text);
+                int running = int.Parse(txtRunning.Text);
+                int calPushUps = pushUps * 7;
+                int calSitUps = sitUps * 3;
+                double calRunning = running * 11.4;
+
+                double total = calPushUps + calSitUps + calRunning;
+                txtPushUps.Clear();
+                txtPushUps.AppendText("0");
+                txtSitUps.Clear();
+                txtSitUps.AppendText("0");
+                txtRunning.Clear();
+                txtRunning.AppendText("0");
+                txtTotalWorkout.Clear();
+                txtTotalWorkout.AppendText(total.ToString());
+            }
+        }
+
+        private void WorkoutCalcForm_Load(object sender, EventArgs e)
+        {
+            txtPushUps.Clear();
+            txtPushUps.AppendText("0");
+            txtSitUps.Clear();
+            txtSitUps.AppendText("0");
+            txtRunning.Clear();
+            txtRunning.AppendText("0");
+        }
     }
 }
